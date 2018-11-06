204722438a7b439c0d977c816d37564a26f05dda
Unknown <vinc.claveau@gmail.com>
2018-11-06 12:50:20 +0100

    Initial Symfony

---
+++ src/Controller/AppController.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * This file is a part of AirHotel-App
+ *
+ * AppController.php
+ *
+ * @author      Vincent CLAVEAU <vinc.claveau@gmail.com>
+ * @copyright   2018 Vincent CLAVEAU
+ * @date        06/11/2018
+ */
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
+
+/**
+ * @Route("/", name="app_")
+ */
+class AppController
+{
+    /**
+     * @Route("/", name="index")
+     */
+    public function index()
+    {
+
+    }
+}
