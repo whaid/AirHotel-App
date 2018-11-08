736e2cf8035043178bc9fafa736f10b2897bf399
RomaricA <romaric.allegrini@campus-igs-toulouse.fr>
2018-11-08 11:56:41 +0100

    Tickets test

--- src/Repository/RoomRepository.php
+++ src/Repository/RoomRepository.php
@@ -14,11 +14,16 @@ namespace App\Repository;
 use App\Entity\Room;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Common\Persistence\ManagerRegistry;
+use Symfony\Component\Validator\Constraints\Date;
 
 class RoomRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Room::class);
+    }
+
+    public function getRoomsDate(Date $dateArrival, Date $dateDeparture){
+        // TODO :
     }
 }
